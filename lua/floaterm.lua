e16c72388c2a4bb84a95a7aca33c7a6c830119ec
pierre <dommerc.pierre@gmail.com>
2021-06-28 23:41:27 +0200

    add rg function

--- lua/floaterm.lua
+++ lua/floaterm.lua
@@ -1,16 +1,15 @@
-local fd_init = require'floaterm.find_file'.init
+local find_file = require'floaterm.find_file'
+local rg = require'floaterm.rg'
 
 local function setup(config)
-  if config.find_file then
-    fd_init(config.find_file)
-  else
-    fd_init(config)
-  end
+  find_file.init(config.find_file or config)
+  rg.init(config.rg or config)
 end
 
 local M = {
   setup = setup,
-  find_file = require'floaterm.find_file'.find_file,
+  find_file = find_file.find_file,
+  rg = rg.rg,
   open = require'floaterm.open'.open,
 }
 
